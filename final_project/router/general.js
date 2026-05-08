4e1ae2bcd5a9a29e191ec4ae16948f9d92230c12
Kuldeep Chudasma <kuldeepchudasama6999@gmail.com>
2026-05-08 17:34:23 +0530

    4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,39 +5,72 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
+public_users.post("/register", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ success: true, books: books });
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn
+
+  const ResutBooks = Object.values(books).find(
+    (e) => e.isbn === isbn
+  );
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+  return res.status(300).json({ success: true, books: ResutBooks });
+});
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const Author = req.params.author
+
+  const ResultBooks = Object.values(books).find(
+    (e) => e.author === Author
+  );
+
+  return res.status(300).json({
+    success: true,
+    data: ResultBooks
+  });
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const Title = req.params.title
+
+   const ResultBooks = Object.values(books).find(
+    (e) => e.title === Title
+  );
+
+  return res.status(300).json({
+    success: true,
+    data: ResultBooks
+  });
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get('/review/:isbn', function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn
+
+   const ResutBooks = Object.values(books).find(
+    (e) => e.isbn === isbn
+  );
+
+  return res.status(300).json({
+    success: true,
+    data: ResutBooks.reviews
+  });
+
 });
 
 module.exports.general = public_users;
